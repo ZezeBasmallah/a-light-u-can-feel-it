c2566d19c4043ae226012e5700d1648016a6fc66
ZezeBasmallah <zezezeko19@gmail.com>
2024-09-03 16:45:34 +0700

    menambahkan file fortpolio.html dan memodifikasi file index,about,styles dan script

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+    document.querySelector('footer').classList.toggle('dark-mode');
+
+    document.querySelectorAll('nav ul li a').forEach(link => {
+        link.classList.toggle('dark-mode');
+    });
+}
+
 function scrollToProfile() {
     window.location.href = "profile.html";
 }
